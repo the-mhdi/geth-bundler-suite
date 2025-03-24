db4526a599a2abd8807e29a4becd79f9f1d44a63
Mehdi karimi <127137582+the-mhdi@users.noreply.github.com>
2025-03-24 15:40:24 +0000

    6

--- bundler/pool/reputation.go
+++ bundler/pool/reputation.go
@@ -1,7 +1,10 @@
 package mempool
 
 import (
+	"bytes"
+	"encoding/gob"
 	"sync"
+	"unsafe"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethdb"
@@ -21,15 +24,15 @@ type ReputationParams struct {
 }
 
 type ReputationManager struct {
+	Reputation map[common.Address]*ReputationParams //entity-> reputaion //for memory cached fast lookup
+	db         ethdb.Database                       //local db
 	mu         sync.RWMutex
-	Reputation map[common.Address]ReputationParams //entity-> reputaion //for memory cached fast lookup
-	db         *ethdb.Database                     //local db
 }
 
 func New(db *ethdb.Database) *ReputationManager {
 
 	return &ReputationManager{
-		Reputation: make(map[common.Address]ReputationParams),
+		Reputation: make(map[common.Address]*ReputationParams),
 		db:         db,
 	}
 }
@@ -38,8 +41,60 @@ func (rm *ReputationManager) UpdateOpsSeen(entity common.Address) error {
 	rm.mu.Lock()
 	defer rm.mu.Unlock()
 
-	if _, ok := rm.Reputation[entity]; !ok {
+	value, ok := rm.Reputation[entity]    //entity already cached in memory
+	has, err := rm.db.Has(entity.Bytes()) //retrieves the entity from persistent database
+
+	if err != nil {
+		return err
+	}
+
+	if ok {
+		//if entity reputation is already loaded in the memory
+		value.OpsSeen++ //increment
+
+		err := rm.db.Put(entity.Bytes(), value.Bytes()) //update db
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if has && !ok {
+		//entity exists in the db but not loaded into memory
+		rp, err := rm.db.Get(entity.Bytes())
+		rm.Reputation[entity] = new(ReputationParams)
 
 	}
 
+	if !ok && !has {
+		//first time the entity being seen
+		rm.Reputation[entity] = new(ReputationParams)
+
+		err := rm.db.Put(entity.Bytes(), new(ReputationParams).Bytes())
+
+		if err != nil {
+			return err
+		}
+
+	}
+
+}
+
+func (rp *ReputationParams) Bytes() []byte {
+	buffer := bytes.NewBuffer(make([]byte, 0, unsafe.Sizeof(rp)))
+	enc := gob.NewEncoder(buffer)
+	enc.Encode(rp)
+
+	return buffer.Bytes()
+
+}
+
+func decode(rp []byte) *ReputationParams {
+	buffer := bytes.NewBuffer(rp)
+
+	dec := gob.NewDecoder(buffer)
+	rpStruct := new(ReputationParams)
+	dec.Decode(rpStruct)
+
+	return rpStruct
 }
