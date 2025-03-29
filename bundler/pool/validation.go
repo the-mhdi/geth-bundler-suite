c7debbb5b5e93621dc6113da9fc7b4b7b44ebe09
Mehdi karimi <the_neve@outlook.com>
2025-03-29 15:04:13 +0000

    AA contracts added

--- bundler/pool/validation.go
+++ bundler/pool/validation.go
@@ -5,8 +5,33 @@ import (
 	"github.com/the-mhdi/geth-bundler-suite/bundler/types"
 )
 
+//implementation of the canonical public ERC-4337 mempool as mentioned in erc-7562
+
+
+type Monitor struct {
+	paymasterGasEstimate map[]
+}
+
+
 type ValidationManager interface {
-	isValid() (bool, error)
-	userOperation() *types.UserOperation
-	atBlockHash() common.Hash
+	GREP() (bool, error) //General Reputation Rules
+	OP() (bool, error)   //Opcode Rules
+	COD() (bool, error)  ///Code Rule
+	STO() (bool, error)  //Storage Rules
+	SREP() (bool, error) //Staked Entities Reputation Rules
+	EREP() (bool, error) //Entity Specific Rules
+	UREP() (bool, error) //Unstaked Paymasters Reputation Rules
+}
+
+
+/**Entity-specific Rules**/
+
+func EREP010(uo *types.UserOperation) {
+/*	For each paymaster, the bundler must track the total gas UserOperations using this paymaster may consume.
+Bundler should not accept a new UserOperation with a paymaster to the mempool if the maximum total gas cost of all userops in the mempool, including this new UserOperation, is above the deposit of that paymaster at the current gas price.
+*/
+	if uo.Paymaster != common.HexToAddress("0x") {
+		
+	}
+uo.Paymaster 
 }
